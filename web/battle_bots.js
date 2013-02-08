934db2e7dffba5a9cdca480c3d6b8b100d4b968b
James Whitwell <jtwhitwell124@gmail.com>
2013-02-09 00:38:18 +1300

    Added movement

--- web/battle_bots.js
+++ web/battle_bots.js
@@ -11,6 +11,25 @@ var worldWidth = 128, worldHeight = 128, worldDepth = 128,
 
 var clock = new THREE.Clock();
 
+var home = {x: 0, y: 0, z:0};
+
+var player = {
+    position: {x: 0, y: 0, z:0},
+    rotation: {x: 0, y: 0, z:0}
+};
+
+var moveState = {
+    moving    : false,
+    front     : false,
+    Backwards : false,
+    left      : false,
+    right     : false,
+    speed     : .1,
+    angle     : 0
+};
+
+var terrainMesh;
+
 function init() {
 
 	container = document.getElementById( 'container' );
@@ -37,6 +56,11 @@ function init() {
 
 			geometry.vertices[ i ].y = data[ i ] * 10;
 
+            if (data[i] > home.y) {
+                home.x = geometry.vertices[i].x;
+                home.y = geometry.vertices[i].y;
+                home.z = geometry.vertices[i].z;
+            }
 		}
 
 		//texture = new THREE.Texture( generateTexture( data, worldWidth, worldDepth ), new THREE.UVMapping(), THREE.ClampToEdgeWrapping, THREE.ClampToEdgeWrapping );
@@ -45,8 +69,8 @@ function init() {
 
 		texture.needsUpdate = true;
 
-		mesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
-		scene.add( mesh );
+		terrainMesh = new THREE.Mesh( geometry, new THREE.MeshBasicMaterial( { map: texture } ) );
+		scene.add( terrainMesh );
 
 		renderer = new THREE.WebGLRenderer();
 		renderer.setSize( window.innerWidth, window.innerHeight );
@@ -76,6 +100,88 @@ function onWindowResize() {
 	controls.handleResize();
 
 }
+
+function movePlayer() {
+
+    var speed = moveState.speed;
+
+    var direction = moveState.angle;
+    if( moveState.front && !moveState.left && !moveState.Backwards && !moveState.right){direction +=   0}
+    if( moveState.front &&  moveState.left && !moveState.Backwards && !moveState.right){direction +=  45}
+    if(!moveState.front &&  moveState.left && !moveState.Backwards && !moveState.right){direction +=  90}
+    if(!moveState.front &&  moveState.left &&  moveState.Backwards && !moveState.right){direction += 135}
+    if(!moveState.front && !moveState.left &&  moveState.Backwards && !moveState.right){direction += 180}
+    if(!moveState.front && !moveState.left &&  moveState.Backwards &&  moveState.right){direction += 225}
+    if(!moveState.front && !moveState.left && !moveState.Backwards &&  moveState.right){direction += 270}
+    if( moveState.front && !moveState.left && !moveState.Backwards &&  moveState.right){direction += 315}
+
+    player.rotation.y = (direction+270) * Math.PI / 180;
+
+    player.direction = direction;
+    player.position.x -= Math.sin(direction * Math.PI / 180) * speed;
+    player.position.z -= Math.cos(direction * Math.PI / 180) * speed;
+
+    var vec = new THREE.Vector3( 0, -1, 0 );
+    var pos = new THREE.Vector3(player.position.x, player.position.y+2, player.position.z);
+    var raycaster = new THREE.Raycaster(pos, vec);
+    var intersects = raycaster.intersectObject(terrainMesh);
+
+    if (intersects.length>0) {
+        player.position.y = intersects[0].point.y+.5;
+    }
+}
+
+var timer;
+
+document.addEventListener('keydown', function(e){
+    if( !/65|68|83|87|38|40|37|39/.test(e.keyCode)){ console.log(e.keyCode); return }
+    if( e.keyCode === 87 | e.keyCode === 38){
+        moveState.front     = true;
+        moveState.Backwards = false;
+    } else if ( e.keyCode === 83 | e.keyCode === 40){
+        moveState.Backwards = true;
+        moveState.front     = false;
+    } else if ( e.keyCode === 65 | e.keyCode === 37){
+        moveState.left  = true;
+        moveState.right = false;
+    } else if ( e.keyCode === 68 | e.keyCode === 39){
+        moveState.right = true;
+        moveState.left  = false;
+    }
+    if(!moveState.moving){
+        if(player.state === 'stand')  {player.changeMotion('run');}
+        if(player.state === 'crstand'){player.changeMotion('crwalk');}
+        moveState.moving = true;
+        movePlayer();
+        timer = setInterval( function(){
+            movePlayer();
+        }, 1000 / 60);
+    }
+}, false);
+
+document.addEventListener('keyup', function(e){
+    if( !/65|68|83|87|38|40|37|39|84/.test(e.keyCode)){ return }
+    if( e.keyCode === 87 | e.keyCode === 38){
+        moveState.front = false;
+    } else if ( e.keyCode === 83 | e.keyCode === 40 ){
+        moveState.Backwards = false;
+    } else if ( e.keyCode === 65 | e.keyCode === 37){
+        moveState.left = false;
+    } else if ( e.keyCode === 68  | e.keyCode === 39){
+        moveState.right = false;
+    } else if (e.keyCode === 84 ) {
+        moveState.front = false;
+        moveState.Backwards = false;
+        moveState.left = false;
+        moveState.right = false;
+       // teleportHome();
+    }
+    if(!moveState.front && !moveState.Backwards && !moveState.left && !moveState.right){
+        player.changeMotion(player.state);
+        moveState.moving = false;
+        clearInterval(timer);
+    }
+}, false);
 
 function generateHeight(heightImg) {
 	var canvas = document.createElement( 'canvas' );
